7531febdc9356f8fcc8f225c5692f40c743c550c
Cocovel <46144174+cocovel@users.noreply.github.com>
2023-11-23 22:45:16 -0600

    update dat.gui -> lil-gui

---
+++ 04.transforms-objects/exercise/src/trie.js
@@ -0,0 +1,34 @@
+//Node
+//Trie:
+// insert
+// contains
+// startsWithPrefix
+class Node {
+  constructor() {
+    this.children = {};
+    this.isWordEnd = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(word) {
+    let curr = this.root;
+    for (let i = 0; i < word.length; i++) {
+      let charToInsert = word[i];
+      if(!(charToInsert in curr.children)){
+        curr.children[charToInsert] = new Node();
+      }
+      curr = curr.children[charToInsert];
+    }
+
+    curr.isWord = true;
+  }
+
+  contains(word) {
+
+  }
+}
